269ae13a477a4ea804080d11539abfb48d4751e0
Justin <justin.scopelleti@gmail.com>
2023-09-17 16:53:25 -0400

    refactor: Migrate To pnpm From yarn + Remove Unused Files

--- src/globalDark.css.ts
+++
@@ -1,8 +0,0 @@
-import { createTheme } from "@vanilla-extract/css";
-import { appTheme } from "~/globalStyles.css";
-import { theme } from "~/utils/constants";
-
-export const darkTheme = createTheme(appTheme, {
-  colors: theme.dark
-});
-
--- src/globalLight.css.ts
+++
@@ -1,7 +0,0 @@
-import { createTheme } from "@vanilla-extract/css";
-import { appTheme } from "~/globalStyles.css";
-import { theme } from "~/utils/constants";
-
-export const lightTheme = createTheme(appTheme, {
-  colors: theme.light
-});
